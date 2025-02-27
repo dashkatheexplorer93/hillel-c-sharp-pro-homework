ada55ad112f382ec3b96084043b4bec7b6ea4e3a
dbab <dbab@jooble.com>
2025-02-27 23:05:35 +0200

    push repo with hw 1

---
+++ Hillel/Calculator.cs
@@ -0,0 +1,20 @@
+namespace Hillel;
+
+public static class Calculator
+{
+    public static double Add(double num1, double num2) => num1 + num2;
+    
+    public static double Subtract(double num1, double num2) => num1 - num2;
+    
+    public static double Multiply(double num1, double num2) => num1 * num2;
+
+    public static double Divide(double num1, double num2)
+    {
+        if (num2 == 0)
+        {
+            throw new DivideByZeroException("Division by 0 is impossible");
+        }
+
+        return num1 / num2;
+    }
+}
---
+++ Hillel/Program.cs
@@ -0,0 +1,51 @@
+﻿using Hillel;
+
+Console.WriteLine("Welcome to simple calculator!");
+
+Console.Write("Enter the first number: ");
+if (!double.TryParse(Console.ReadLine(), out var num1))
+{
+      Console.WriteLine("Invalid input for the first number");
+      return;
+}
+
+Console.WriteLine("Choose the operation: +, -, *, / ");
+var input = Console.ReadLine();
+if (input.Length > 1)
+{
+    Console.WriteLine("Invalid operation");
+    return;
+}
+var operation = char.Parse(input);
+
+Console.Write("Enter the second number: ");
+if (!double.TryParse(Console.ReadLine(), out var num2))
+{
+    Console.WriteLine("Invalid input for the second number");
+    return;
+}
+
+try
+{
+    switch (operation)
+    {
+        case '+':
+            Console.WriteLine($"The result is: {Calculator.Add(num1, num2)}");
+            break;
+        case '-':
+            Console.WriteLine($"The result is: {Calculator.Subtract(num1, num2)}");
+            break;
+        case '*':
+            Console.WriteLine($"The result is: {Calculator.Multiply(num1, num2)}");
+            break;
+        case '/':
+            Console.WriteLine($"The result is: {Calculator.Divide(num1, num2)}");
+            break;
+        default:
+            throw new InvalidOperationException("Invalid operation");
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Error: {ex.Message}");
+}
